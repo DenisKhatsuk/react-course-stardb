1227bbcbdb00be1957ebec5a76db54ce7b04c883
Dzianis Khatsuk <dennis.hatsuk@gmail.com>
2021-03-12 10:42:47 +0300

    init: start StarDB app for React+Redux course

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const luke = 'https://swapi.dev/api/people/1';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const getResource = async (url) => {
+  const res = await fetch(url);
+  const body = await res.json();
+  return body;
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+getResource(luke)
+  .then(body => console.log(body));
