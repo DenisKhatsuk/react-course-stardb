cd5f5de3de1e0afc6d58855904a38d625c5fc9d8
Dzianis Khatsuk <dennis.hatsuk@gmail.com>
2021-03-12 23:15:15 +0300

    feat: add SwapiService class

--- src/index.js
+++ src/index.js
@@ -1,10 +1,50 @@
-const luke = 'https://swapi.dev/api/people/1';
+class SwapiService {
+  _apiBase = 'https://swapi.dev/api';
 
-const getResource = async (url) => {
-  const res = await fetch(url);
-  const body = await res.json();
-  return body;
-};
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+    if (!res.ok) throw new Error(`Could not fetch ${this._apiBase}${url}. Server respond status ${res.status}.`);
+    const body = await res.json();
+    return body;
+  }
 
-getResource(luke)
-  .then(body => console.log(body));
+  async getAllPeople() {
+    const res = await this.getResource('/people/');
+    return res.results;
+  }
+  
+  getPerson(id) {
+    return this.getResource(`/people/${id}`);
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResource('/planets/');
+    return res.results;
+  }
+  
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}`);
+  }
+
+  async getAllStarships() {
+    const res = await this.getResource('/starships/');
+    return res.results;
+  }
+  
+  getStarship(id) {
+    return this.getResource(`/starships/${id}`);
+  }
+}
+
+const swapi = new SwapiService();
+
+swapi.getAllPeople()
+  .then((body) => {
+    body.forEach(p => console.log(p.name));
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+swapi.getPerson(5)
+  .then(p => console.log(p.name));
