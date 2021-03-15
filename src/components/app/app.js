5e5eda11c4d21c69e1852f24c5dc71d3264c4c59
Dzianis Khatsuk <dennis.hatsuk@gmail.com>
2021-03-15 21:51:01 +0300

    fix: remove unecessary imports from app component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
 import ErrorButton from '../error-button';
 import ErrorIndicator from '../error-indicator';
 import PeoplePage from '../people-page';
