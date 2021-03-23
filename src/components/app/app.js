255774beb4194dbeb5f20623e5f661674f0137c4
Dzianis Khatsuk <dennis.hatsuk@gmail.com>
2021-03-24 00:40:51 +0300

    feat: add People page header

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -58,6 +58,10 @@ export default class App extends Component {
                 path = "/" 
                 render = { () => <h2>Welcome to Star DB</h2> }
                 exact />
+              <Route 
+                path = "/people" 
+                render = { () => <h2>Star wars characters</h2> }
+                exact />
               <Route path = "/people" component = { PeoplePage } />
               <Route path = "/planets" component = { PlanetsPage } />
               <Route path = "/starships" component = { StarshipsPage } />
